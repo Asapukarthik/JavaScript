82b0f34f220636564062b1759eb3bbda89ef0673
AsapuKarthik <karthikasapu21@gmail.com>
2025-01-02 20:11:31 +0530

    basic JS

---
+++ 01_Variables.js
@@ -0,0 +1,17 @@
+// LET -  VARIABLE CANNOT BE RE-DECLARED BUT CAN BE UPDATED
+
+let age=10;
+console.log(`age ${age}`);
+
+// VAR - VARIABLE CAN BE RE-DECLARED & UPDATED 
+
+var x=10;
+console.log(`x value ${x}`);
+x=20;
+console.log(`x value ${x}`);
+
+
+// CONST - VARIABLE CANNOT BE RE-DECLARED & UPDATED\
+
+const value=10;
+console.log(`value is ${value}`);
